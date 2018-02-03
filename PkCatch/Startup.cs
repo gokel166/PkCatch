8272914155291df963901635a6710900b8258219
George Okello <okellogeorge40@yahoo.com>
2018-02-02 23:31:30 -0600

    New commit

--- PkCatch/Startup.cs
+++ PkCatch/Startup.cs
@@ -25,6 +25,7 @@ namespace PkCatch
             services.AddMvc();
         }
 
+        //
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
